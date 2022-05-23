ce73a36ae166053427cbc56bebefd53ad8af96ee
Sourabh Tiwari <sourabh.a.tiwari@accenture.com>
2022-05-24 01:06:39 +0530

    Added new problem

--- Program.cs
+++ Program.cs
@@ -49,13 +49,24 @@ namespace ProblemPractise
             //var ans = mp.CountOfDivisors(A);
 
 
-            HashingProblems hp = new HashingProblems();
-            List<int> A = new List<int>(new List<int> { 5, 4, 10, 15, 7, 6 });
-            int B = 5;
+            //HashingProblems hp = new HashingProblems();
+            //List<int> A = new List<int>(new List<int> { 5, 4, 10, 15, 7, 6 });
+            //int B = 5;
 
             //var ans = hp.SubarrayWithGivenSum(A, 110);
             //var ans = hp.ContiguousArray(A);
-            var ans = hp.PairsWithGivenXor(A, B);
+            //var ans = hp.PairsWithGivenXor(A, B);
+
+
+            SearchingProblems search = new SearchingProblems();
+            //List<int> A = new List<int>(new List<int> { 1, 3, 5, 6 });
+            //int B = 4;
+
+            List<int> A = new List<int>(new List<int> { 3, 4, 18, 19, 20, 27, 28, 31, 36, 42, 44, 71, 72, 75, 82, 86, 88, 97, 100, 103, 105, 107, 110, 116, 118, 119, 121, 122, 140, 141, 142, 155, 157, 166, 176, 184, 190, 199, 201, 210, 212, 220, 225, 234, 235, 236, 238, 244, 259, 265, 266, 280, 283, 285, 293, 299, 309, 312, 317, 335, 341, 352, 354, 360, 365, 368, 370, 379, 386, 391, 400, 405, 410, 414, 416, 428, 433, 437, 438, 445, 453, 457, 458, 472, 476, 480, 485, 489, 491, 493, 501, 502, 505, 510, 511, 520, 526, 535, 557, 574, 593, 595, 604, 605, 612, 629, 632, 633, 634, 642, 647, 653, 654, 656, 658, 686, 689, 690, 691, 709, 716, 717, 737, 738, 746, 759, 765, 775, 778, 783, 786, 787, 791, 797, 801, 806, 815, 820, 822, 823, 832, 839, 841, 847, 859, 873, 877, 880, 886, 904, 909, 911, 917, 919, 937, 946, 948, 951, 961, 971, 979, 980, 986, 993 });
+            int B = 902;
+
+            //var ans = search.SearchSingleElement(A);
+            var ans = search.searchInsert(A,B);
 
             //ans.ForEach(x => Console.WriteLine("Output:" + x));
             Console.WriteLine("Output: "+ans);
---
+++ SearchingProblems.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemPractice
+{
+    internal class SearchingProblems
+    {
+        //Single Element in a Sorted Array
+        public int SearchSingleElement(List<int> A)
+        {
+            int ans = 0, n = A.Count;
+            int low = 0, high = n - 1;
+            while (low <= high)
+            {
+                int mid = (high - low) / 2 + low;
+                if (mid == n - 1)
+                    return A[n - 1];
+                if (A[mid] == A[mid + 1])
+                    mid++;
+                // if all elements with index < mid are occuring twice then mid should be odd
+                if (mid % 2 == 1)
+                    low = mid + 1;
+                else
+                {
+                    ans = mid;
+                    high = mid - 1;
+                }
+            }
+            return A[ans];
+        }
+        //Sorted Insert Position       
+        public int searchInsert(List<int> A, int B)
+        {
+            int ans = 0, n = A.Count;
+            int low = 0, high = n - 1;
+            if (A[low] == B)
+                return low;
+            if(A[high] == B)
+                return high;
+            while (low <= high)
+            {
+                int mid = (high - low) / 2 + low;
+                if (A[mid] == B)
+                    return mid;               
+                // if all elements with index < mid are occuring twice then mid should be odd
+                if (A[mid] < B)
+                    low = mid + 1;
+                else
+                {
+                    ans = mid;
+                    high = mid - 1;
+                }
+            }
+            if (ans == 0 && A[n - 1] < B)
+                ans = n;
+            return ans;
+        }
+    }
+}
